dbfa7f20e1a3b3a91598916fb871109eeac1d6bb
João Lopes <jvianalopes@gmail.com>
2018-04-02 17:46:56 +0100

    Added Vacancies in C++; corrected two bugs: Simulation.hpp and export_lattice.py

---
+++ Src/HamiltonianVacancies.hpp
@@ -0,0 +1,80 @@
+template <typename T,unsigned D>
+struct Vacancy_Operator {
+  LatticeStructure <D>                   & r;
+  Simulation <T,D>                       & simul;
+  // vector of vectors with positions in the lattice of Vacancies for each stride block
+  std::vector <std::vector<std::size_t>> position;                   
+  std::vector <double>                   concentration;
+  std::vector <std::vector<int>>         orbitals;
+  std::vector <std::size_t>              vacancies_with_defects; 
+  
+  Vacancy_Operator(Simulation <T,D> & sim) : r(sim.r), simul(sim), position(sim.r.NStr)
+  {
+  };
+  
+  
+  void generate_disorder()
+  {
+    Coordinates<std::size_t,D + 1> latt(r.ld), Latt(r.Ld), latStr(r.lStr);
+    
+    // Clean former vacancy distribution
+    for(unsigned i = 0; i < r.NStr ; i++)
+      position.at(i).clear();
+    vacancies_with_defects.clear();
+    // Distribute Vacancies
+    
+    for( std::size_t i = 0; i < r.N ; i++)
+      for(unsigned j = 0; j < concentration.size(); j++)
+	for(unsigned k = 0; k < concentration.size(); k++)
+	  if(simul.rnd.get() < concentration[k])
+	    for(auto o = orbitals.at(k).begin(); o != orbitals.at(k).end(); o++)
+	      {
+		latt.set_coord(i + std::size_t(*o) * r.N );         // Set Vacancy position inside the domain
+		r.convertCoordinates(latStr,latt);                  // Get stride position
+		r.convertCoordinates(Latt,latt);                    // Get Domain coordinates
+		position.at(latStr.index).push_back(Latt.index);    // store the position by stride 
+	      }
+  };
+  
+  void add_model(double p, std::vector <int> & orb)
+  {
+    orbitals.push_back(orb);
+    concentration.push_back(p);
+  }
+  
+  void add_conflict_with_defect(std::size_t element, unsigned istride)
+  {
+    std::vector<std::size_t> & v = position.at(istride);  
+    for(unsigned i = 0; i < v.size(); i++)
+      if(element == v.at(i))
+	vacancies_with_defects.push_back(element);
+  }
+  
+  bool test_vacancy(Coordinates<std::size_t,D + 1> & Latt)
+  {
+    /*
+      1 if is a vacancy
+      0 if not
+    */
+    Coordinates<std::size_t,D + 1> latStr(r.lStr);
+    r.convertCoordinates(latStr, Latt);
+    auto & vc = position.at(latStr.index);
+    if( !any_of(vc.begin(), vc.end(), std::bind2nd(std::equal_to<std::size_t>(), Latt.index)))
+      return 0;
+    else
+      return 1;
+  }
+
+  void test_field( T * phi0 )
+  {
+    // The field should be zero in the vacancies
+    for(unsigned i = 0; i  < r.NStr; i++)
+      for(auto vc = position.at(i).begin(); vc != position.at(i).end(); vc++)
+	if( abs(phi0[*vc]) > __DBL_EPSILON__)
+	  {
+	    std::cout << "Disparate" << std::endl;
+	    exit(0);
+	  }
+  }
+  
+};
---
+++ example5.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import export_lattice as ex
+import numpy as np
+import pybinding as pb
+
+energy_scale = 3.06
+def graphene_initial(onsite=(0, 0)):
+    theta = np.pi / 3
+    a1 = np.array([1 + np.cos(theta), np.sin(theta)])
+    a2 = np.array([0, 2 * np.sin(theta)])
+
+    lat = pb.Lattice(
+        a1=a1,
+        a2=a2
+    )
+
+    lat.add_sublattices(
+        # name, position, and onsite potential
+        ('A', [0, 0], onsite[0]),
+        ('B', [1, 0], onsite[1])
+    )
+
+    lat.add_hoppings(
+        ([-1, 0], 'A', 'B', - 1 / energy_scale),
+        ([-1, 1], 'A', 'B', - 1 / energy_scale)
+    )
+
+    disorder = ex.Disorder(lat)
+    disorder.add_disorder('A', 'Deterministic', 0.0, 0)
+    disorder.add_disorder('B', 'Deterministic', 0.0, 0)
+    
+    struc_disorder = ex.StructuralDisorder(lat, concentration=0.004)
+    struc_disorder.add_vacancy('A')
+    struc_disorder.add_vacancy('B')
+    
+    return lat, disorder, [struc_disorder]
+
+
+lattice, disorder, disorded_structural = graphene_initial()
+
+nx = ny = 1
+lx = 256
+ly = 256
+
+configuration = ex.Configuration(divisions=[nx, ny], length=[lx, ly], boundaries=[True, True],
+                                 is_complex=False, precision=1, energy_scale=energy_scale)
+
+NPontos = 100
+M = 2048
+g1 = 0.030
+calculation = ex.Calculation(fname=['DOS','SingleCondXX'],
+                             num_moments=[M,M], num_random=[1,1], 
+                             num_disorder=[1,1], 
+                             energy=[[0.3/NPontos * i/energy_scale  for i in range(NPontos)]], 
+                             gamma=[[g1/energy_scale]])
+
+modification = ex.Modification(magnetic_field=False)
+ex.export_lattice(lattice, configuration, calculation, modification, 'example5.h5',
+                  disorder=disorder, disorded_structural=disorded_structural)
+
