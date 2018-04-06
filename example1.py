a24017a55ace926d3e47dd5aec91cfc0d61fad74
Misa <misa.s.andelkovic@gmail.com>
2018-04-06 15:14:50 +0200

    Update vacancy example energy scaling

--- example1.py
+++ example1.py
@@ -3,11 +3,10 @@ import export_lattice as ex
 import numpy as np
 import pybinding as pb
 
-
 # define lattice of monolayer graphene with 1[nm] interatomic distance and t=1/3[eV] hopping,
 # EnergyScale is the scaling factor of the hopping parameters, important for the rescaling of the spectral quantity.
 #  INFO: other examples are defined in define_lattice.py script
-energy_scale = 3.06
+energy_scale = 6.06
 
 
 def graphene_initial(onsite=(0, 0)):
@@ -26,8 +25,8 @@ def graphene_initial(onsite=(0, 0)):
     # Add sublattices
     lat.add_sublattices(
         # name, position, and onsite potential
-        ('A', [0, 0], onsite[0]),
-        ('B', [1, 0], onsite[1])
+        ('A', [0, 0], onsite[0] / energy_scale),
+        ('B', [1, 0], onsite[1] / energy_scale)
     )
 
     # Add hoppings
@@ -46,21 +45,53 @@ def graphene_initial(onsite=(0, 0)):
     # of deterministic disorder should be 0.
 
     disorder = ex.Disorder(lat)
-    disorder.add_disorder('A', 'Deterministic', 0.0, 0)
-    disorder.add_disorder('B', 'Deterministic', 0.0, 0)
+    disorder.add_disorder('A', 'Gaussian', 0.5 / energy_scale, 0.1)
+    disorder.add_disorder('B', 'Uniform', 0.2 / energy_scale, 0.1)
 
     # Add bond disorder as an object of a class StructuralDisorder. In this manner we can add onsite and bond defects
     # with a specific concentration, which will be added to the simulated system. The procedure for adding is same
     # as adding the hopping, with the difference that the bond disorded is not bounded to one site in the [0, 0]
     # unit cell.
-    
+    node0 = [[+0, +0], 'A']
+    node1 = [[+0, +0], 'B']
+    node2 = [[+1, +0], 'A']
+    node3 = [[+0, +1], 'B']
+    node4 = [[+0, +1], 'A']
+    node5 = [[-1, +1], 'B']
+    node6 = [[-2, +2], 'B']
+    struc_disorder_one = ex.StructuralDisorder(lat, concentration=0.05)
+    struc_disorder_one.add_structural_disorder(
+        # add bond disorder in the form [from unit cell], 'sublattice_from', [to_unit_cell], 'sublattice_to', value:
+        (*node0, *node1, 1 / energy_scale),
+        (*node1, *node2, 1 / energy_scale),
+        (*node2, *node3, 1 / energy_scale),
+        (*node3, *node4, 1 / energy_scale),
+        (*node4, *node5, 1 / energy_scale),
+        (*node5, *node0, 1 / energy_scale),
+        # in this way we can add onsite disorder in the form [unit cell], 'sublattice', value
+        ([+0, +0], 'B', 0.1 / energy_scale)
+    )
+    # It is possible to add multiple different disorder type which should be forwarded to the export_lattice function
+    # as a list.
+    struc_disorder_two = ex.StructuralDisorder(lat, concentration=0.2)
+    struc_disorder_two.add_structural_disorder(
+        (*node0, *node1, 0.1 / energy_scale),
+        (*node4, *node5, 0.1 / energy_scale),
+        (*node5, *node0, 0.1 / energy_scale),
+        ([+0, +0], 'B', 0.1 / energy_scale)
+    )
+    struc_disorder_two.add_vacancy('B')
+
+    struc_disorder_three = ex.StructuralDisorder(lat, concentration=0.01)
+    struc_disorder_three.add_vacancy('A')
+
     # if there is disorder it should be returned separately from the lattice
-    return lat, disorder, []
+    return lat, disorder, [struc_disorder_one, struc_disorder_two, struc_disorder_three]
 
 
 lattice, disorder, disorded_structural = graphene_initial()
-
-# number of decomposition parts in each direction of matrix. This divides the lattice into various sections, each of which is calculated in parallel
+# number of decomposition parts in each direction of matrix.
+# This divides the lattice into various sections, each of which is calculated in parallel
 nx = ny = 1
 
 # number of unit cells in each direction.
