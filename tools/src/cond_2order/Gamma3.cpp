7e194f582c5c3890398ed3faeb18ef20ee438b1c
Nikolas Garofil <nikolas@garofil.be>
2019-11-09 19:26:31 +0100

    Cleanup Gamma3.cpp for pedantic: Remove unused thread_num

--- tools/src/cond_2order/Gamma3.cpp
+++ tools/src/cond_2order/Gamma3.cpp
@@ -32,7 +32,6 @@ Eigen::Matrix<std::complex<U>, -1, -1> conductivity_nonlinear<U, DIM>::Gamma3shg
   // number of threads, thread number and number of moments alocated
   // to each thread. These have to be computed inside a threaded block
   int N_threads;
-  int thread_num;
   int local_NumMoments;
 
   // Functions that are going to be used by the contractor
@@ -143,7 +142,6 @@ Eigen::Matrix<std::complex<U>, -1, -1> conductivity_nonlinear<U, DIM>::Gamma3shg
   // number of threads, thread number and number of moments alocated
   // to each thread. These have to be computed inside a threaded block
   int N_threads;
-  int thread_num;
   int local_NumMoments;
 
   // Functions that are going to be used by the contractor
@@ -256,7 +254,6 @@ Eigen::Matrix<std::complex<U>, -1, -1> conductivity_nonlinear<U, DIM>::Gamma3shg
   // number of threads, thread number and number of moments alocated
   // to each thread. These have to be computed inside a threaded block
   int N_threads;
-  int thread_num;
   int local_NumMoments;
 
   // Functions that are going to be used by the contractor
