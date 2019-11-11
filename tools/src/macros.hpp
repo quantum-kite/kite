720bf037a6b0e70a58072fcb76174d6b52401d4a
Nikolas Garofil <nikolas@garofil.be>
2019-11-11 20:02:56 +0100

    Improve macros.hpp:
    - Rename outcol/outres to reasonable names
    - Define pi and calculate 1/(2*pi) instead of hardcoding
     (Calculations will be done at compiletime, so no loss in speed)
    - VERBOSE and DEBUG will be set to 1 if not defined so no there is no
    need to do a #ifdef for those

--- tools/src/macros.hpp
+++ tools/src/macros.hpp
@@ -1,5 +1,5 @@
-#define outcol "\033[1;31m"
-#define outres "\033[0m"
+#define TEXTCOLOR_RED "\033[1;31m"
+#define TEXTCOLOR_RESET "\033[0m"
 
 #ifndef DEBUG
 #define DEBUG 1
@@ -9,29 +9,22 @@
 #define VERBOSE 1
 #endif
 
+#define PI 3.14159265
 // The first scale is 1/2pi and corresponds to the conductance quantum per spin e^2/h
 // The second scale is the universal conductivity of graphene e^2/4h_bar
-#define scale1 0.159154943
+#define scale1 1/(2*PI)
 #define scale2 0.25
-#define unit_scale 0.159154943
+#define unit_scale 1/(2*PI)
 
-#ifdef VERBOSE
-	#if VERBOSE==1
-		#define verbose_message(VAR) std::cout<<VAR<<std::flush
-	#else
-		#define verbose_message(VAR) 
-	#endif
+#if VERBOSE==1
+	#define verbose_message(VAR) std::cout<<VAR<<std::flush
 #else
 	#define verbose_message(VAR) 
 #endif
 
-#ifdef DEBUG
-	#if DEBUG==1
-		#define debug_message(VAR) std::cout << outcol << VAR << outres << std::flush
-		#define verbose_message(VAR) std::cout<<VAR<<std::flush
-	#else
-		#define debug_message(VAR) 
-	#endif
+#if DEBUG==1
+	#define debug_message(VAR) std::cout << TEXTCOLOR_RED << VAR << TEXTCOLOR_RESET << std::flush
+	#define verbose_message(VAR) std::cout<<VAR<<std::flush
 #else
 	#define debug_message(VAR) 
 #endif
