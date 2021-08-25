5ed51d8aa54f22e9cd4d8d25fa414f9157b7f67c
Edward Higgins <ed.higgins@york.ac.uk>
2021-08-25 10:51:23 +0100

    Adds setup procedure for kite.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kite',
+      version='1.1',
+      description='Fork of KITE',
+      author='Ed Higgins',
+      author_email='ed.higgins@york.ac.uk',
+      url='https://github.com/ejh516/kite',
+      packages=['kite'],
+     )
