1f9b9490adfd7346ece7aed2091f3bf838272730
Ed Higgins <32080365+ejh516@users.noreply.github.com>
2021-10-25 16:28:07 +0100

    Updates description and url in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='kite',
-      version='1.1',
-      description='Fork of KITE',
-      author='Ed Higgins',
+      version='1.0',
+      description='Quantum KITE is an open-source software for accurate real space evaluations of electronic structure and response functions of large-scale tight-binding models',
+      author='Edi Hggins',
       author_email='ed.higgins@york.ac.uk',
-      url='https://github.com/ejh516/kite',
+      url='https://github.com/quantum-kite/kite',
       packages=['kite'],
      )
