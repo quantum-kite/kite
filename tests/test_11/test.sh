cba1cde271b2d9c6b8ab8e973ced6237e1233d1e
Simão Meneses João <simao_meneses_joao@hotmail.com>
2022-01-09 22:55:28 +0000

    added test for custom local energy

--- tests/test_11/test.sh
+++ tests/test_11/test.sh
@@ -8,23 +8,27 @@ file=/Calculation/dos/MU
 
 
 
+dir=../tests/next_14_customlocal
 
 if [[ "$1" == "redo" ]]; then
     
     # Replace the library in the lib directory
-    rm ../../lib/libaux.so.1
-    ln -s libaux.so ../tests/next_14_customlocal/libaux.so.1
-    cp libaux.so.1 ../../lib/
+    cd ../../lib
+    rm libaux.so.1
+    ln -s $dir/libaux.so libaux.so.1
+    cd $dir
 
     # Use the existing configuration file
-    #cp configORIG.h5 config.h5
-    #chmod 755 config.h5
-    #SEED=$S ../KITEx config.h5 > log_KITEx
-    #python ../compare.py configREF.h5 $file config.h5 $file
+    cp configORIG.h5 config.h5
+    chmod 755 config.h5
+    SEED=$S ../KITEx config.h5 > log_KITEx
+    python ../compare.py configREF.h5 $file config.h5 $file
 
     ## Put the library back the way it was, so it does not change the other tests
-    #rm ../../lib/libaux.so.1
-    #ln -s ../../lib/libaux.so ../../lib/libaux.so.1
+    cd ../../lib
+    rm libaux.so.1
+    ln -s libaux.so libaux.so.1
+    cd $dir
 fi
 
 if [[ "$1" == "script" ]]; then
