1f85c68b9ad21467ed6c70abc012013f566e207d
João Manuel Viana Parente Lopes <jvianalopes@gmail.com>
2022-07-09 15:08:34 +0100

    Changed spectrum_bound to spectrum_range

--- examples/twisted_bilayer.py
+++ examples/twisted_bilayer.py
@@ -46,7 +46,7 @@ lattice = pb.load(name)
 # - boundary conditions, setting True as periodic boundary conditions, and False elsewise,
 # - info if the exported hopping and onsite data should be complex,
 # - info of the precision of the exported hopping and onsite data, 0 - float, 1 - double, and 2 - long double.
-# - scaling, if None it's automatic, if present select spectrum_bound=[e_min, e_max]
+# - scaling, if None it's automatic, if present select spectrum_range=[e_min, e_max]
 configuration = kite.Configuration(divisions=[nx, ny], length=[l1, l2], boundaries=[True, True],
                                    is_complex=False, precision=1, spectrum_range=[-10, 10])
 # require the calculation of DOS
