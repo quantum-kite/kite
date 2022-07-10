68ec50b075a53e82b60f2d4216a4ec1d8c2dca1e
Aires <aires.f.ferreira@gmail.com>
2022-07-10 19:10:30 +0000

    Updated

--- examples/basic_checkboard_lattice.py
+++ examples/basic_checkboard_lattice.py
@@ -9,13 +9,13 @@
         #                                                                            #      
         ##############################################################################      
 """
-""" checkboard lattice
+""" Density of states of a checkboard model 
 
-    Lattice : Square 1[nm] interatomic distance and t=1[eV] hopping;
-    Configuration : size of the system 512x512, with domain decomposition (nx=ny=2), periodic boundary conditions,
-                    double precision, automatic scaling;
-    Calculation : dos;
-
+    Units: Energy in units of hopping, |t| = 1
+    Lattice: Square lattice
+    Configuration: Twisted boundary conditions, double precision, automatic rescaling
+    Calculation type: Average DOS
+    Last updated: 10/07/2022
 """
 
 import kite
@@ -23,45 +23,47 @@ import numpy as np
 import pybinding as pb
 
 def checkboard_lattice(onsite=(0, 0)):
-    """Return the lattice specification for monolayer graphene"""
-    a = 1   # [nm] unit cell length
-    t = 1      # [eV] nearest neighbour hopping
-    delta=0
+    # Return lattice specification for a checkboard model with nearest neighbor hoppings
+
+    a1=np.array([1, 0])
+    a2=np.array([0, 1])
+    
     # create a lattice with 2 primitive vectors
-    lat = pb.Lattice(
-        a1=[a, 0],
-        a2=[0, a]
-    )
+    lat = pb.Lattice(a1=a1, a2=a2)
+    
+    # add sublattices
 
-    lat.add_sublattices(
-        # name and position
-        ('A', [0,  0], onsite[0]),
-        ('B', [a/2,  a/2], onsite[1])
-    )
+    lat.add_sublattices(('A', [0,  0], onsite[0]),('B', [1/2,  1/2], onsite[1]))
 
+    # Add hoppings
     lat.add_hoppings(
         # inside the main cell
-        ([0,  0], 'A', 'B',t),
+        ([0,  0], 'A', 'B', -1),
         # between neighboring cells
-        ([-1, 0], 'A', 'B',  t),
-        ([0, -1], 'A', 'B',  t),
-        ([-1, -1], 'A', 'B', t)
-    )
+        ([-1, 0], 'A', 'B',  -1),
+        ([0, -1], 'A', 'B',  -1),
+        ([-1, -1], 'A', 'B', -1))
 
     return lat
 
-
-
-# load a square lattice
+# load lattice
 lattice = checkboard_lattice((-0.1,0.1))
-# number of decomposition parts in each direction of matrix. This divides the lattice into various sections,
+# number of decomposition parts [nx,ny,nz] in each direction of matrix. This divides the lattice into various sections,
 # each of which is calculated in parallel
 nx = ny = 2
 # number of unit cells in each direction.
 lx = ly = 512
-configuration = kite.Configuration(divisions=[nx, ny], length=[lx, ly], boundaries=["periodic", "periodic"],
-                                   is_complex=False, precision=1)
-# require the calculation of DOS
+
+# - boundary conditions [mode,mode, ... ] with modes:
+#   . "periodic"
+#   . "open"
+#   . "twist_fixed" -- this option needs the extra argument ths=[phi_1,..,phi_DIM] where phi_i \in [0, 2*M_PI]  
+#   . "twist_random"
+# Boundary Mode
+mode = "periodic"
+
+configuration = kite.Configuration(divisions=[nx, ny], length=[lx, ly], boundaries=[mode,mode], is_complex=False, precision=1)
+# specify calculation type
 calculation = kite.Calculation(configuration)
 calculation.dos(num_points=1000, num_moments=512, num_random=5, num_disorder=1)
 # configure the *.h5 file
