7ecf9f7783ac7f7ba109e5dda67ce88819130908
Aires <aires.f.ferreira@gmail.com>
2022-07-10 12:43:55 +0000

    Updated

--- examples/CubicLatticeDos.py
+++ examples/CubicLatticeDos.py
@@ -9,27 +9,26 @@
         #                                                                            #      
         ##############################################################################      
 """
-""" Square lattice
+""" Cubic lattice with twisted boundary conditions
 
-    Lattice : Square 1[nm] interatomic distance and t=1[eV] hopping;
-    Configuration : size of the system 512x512, with domain decomposition (nx=ny=2), periodic boundary conditions,
-                    double precision, automatic scaling;
-    Calculation : dos;
-
+    Units: arbitrary (energy in units of hopping, |t| = 1)
+    Lattice: Simple cubic lattice
+    Configuration: L_x=L_y=L_z=32, twisted boundary conditions, double precision, automatic Hamiltonian rescaling;
+    Calculation type: Average DOS;
+    Last updated: 10/07/2022
 """
 
 import kite
 import numpy as np
 import pybinding as pb
 
-
 def cubic_lattice(onsite=(0, 0, 0)):
-    """Define a cubic lattice with nearest neighbor hoppings"""
+    # Return lattice specification for a cubic lattice with nearest neighbor hoppings
     a1 = np.array([1, 0, 0])
     a2 = np.array([0, 1, 0])
     a3 = np.array([0, 0, 1])
 
-    # create a lattice with 3 primitive vectors
+    # create lattice with 3 primitive vectors
     lat = pb.Lattice(a1=a1, a2=a2, a3=a3)
 
     # Add sublattices
@@ -41,21 +40,29 @@ def cubic_lattice(onsite=(0, 0, 0)):
                      ([0, 0, 1], 'A', 'A', - 1))
     return lat
 
-
-# load a square lattice
+# load lattice
 lattice = cubic_lattice()
-# number of decomposition parts in each direction of matrix. This divides the lattice into various sections,
+# number of decomposition parts [nx,ny,nz] in each direction of matrix. This divides the lattice into various sections,
 # each of which is calculated in parallel
 nx = ny = nz = 1
 # number of unit cells in each direction.
 lx = ly = lz = 32
-# Twists in Each Direction
-twsx = twsy = twsz = np.pi/2.0
+
+# - boundary conditions [mode,mode, ... ] with modes:
+#   . "periodic"
+#   . "open"
+#   . "twist_fixed"     this option needs the extra argument ths=[phi_1,..,phi_DIM] where phi_i \in [0, 2*M_PI]  
+#   . "twist_random"
 # Boundary Mode
 mode = "random"
+# Twists in Each Direction
+twsx = twsy = twsz = np.pi/2.0
+
+# - specify precision of the exported hopping and onsite data, 0 - float, 1 - double, and 2 - long double.
+# - scaling, if None it's automatic, if present select spectrum_range=[e_min, e_max]
 
 configuration = kite.Configuration(divisions=[nx, ny, nz], length=[lx, ly, lz], boundaries=[mode,mode,mode], is_complex=True, precision=1,angles=(twsx, twsy, twsz))
-# require the calculation of DOS
+# require the calculation of average DOS
 calculation = kite.Calculation(configuration)
 calculation.dos(num_points=4000, num_moments=256, num_random=256, num_disorder=1)
 # configure the *.h5 file
