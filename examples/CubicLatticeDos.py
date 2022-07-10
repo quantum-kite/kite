aff5b4581a4a85d072620c6a7f05460d9a3e4d60
Aires <aires.f.ferreira@gmail.com>
2022-07-10 13:27:45 +0000

    Updated

--- examples/CubicLatticeDos.py
+++ examples/CubicLatticeDos.py
@@ -51,18 +51,18 @@ lx = ly = lz = 32
 # - boundary conditions [mode,mode, ... ] with modes:
 #   . "periodic"
 #   . "open"
-#   . "twist_fixed"     this option needs the extra argument ths=[phi_1,..,phi_DIM] where phi_i \in [0, 2*M_PI]  
+#   . "twist_fixed" -- this option needs the extra argument ths=[phi_1,..,phi_DIM] where phi_i \in [0, 2*M_PI]  
 #   . "twist_random"
 # Boundary Mode
 mode = "random"
-# Twists in Each Direction
+# Twists in each direction
 twsx = twsy = twsz = np.pi/2.0
 
 # - specify precision of the exported hopping and onsite data, 0 - float, 1 - double, and 2 - long double.
 # - scaling, if None it's automatic, if present select spectrum_range=[e_min, e_max]
 
 configuration = kite.Configuration(divisions=[nx, ny, nz], length=[lx, ly, lz], boundaries=[mode,mode,mode], is_complex=True, precision=1,angles=(twsx, twsy, twsz))
-# require the calculation of average DOS
+# specify calculation type
 calculation = kite.Calculation(configuration)
 calculation.dos(num_points=4000, num_moments=256, num_random=256, num_disorder=1)
 # configure the *.h5 file
