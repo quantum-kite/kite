8cf4273cacd7355858420969e48426608259f2e6
Aires <aires.f.ferreira@gmail.com>
2022-07-10 13:24:48 +0000

    Updated

--- examples/SquareLatticeDos.py
+++ examples/SquareLatticeDos.py
@@ -9,22 +9,21 @@
         #                                                                            #      
         ##############################################################################      
 """
-""" Square lattice
+""" Density of states of a square lattice model 
 
-    Lattice : Square 1[nm] interatomic distance and t=1[eV] hopping;
-    Configuration : size of the system 512x512, with domain decomposition (nx=ny=2), periodic boundary conditions,
-                    double precision, automatic scaling;
-    Calculation : dos;
-
+    Units: arbitrary (energy in units of hopping, |t| = 1)
+    Lattice: Square lattice
+    Configuration: Twisted boundary conditions, double precision, automatic Hamiltonian rescaling;
+    Calculation type: Average DOS;
+    Last updated: 10/07/2022
 """
 
 import kite
 import numpy as np
 import pybinding as pb
 
-
 def square_lattice(onsite=(0, 0)):
-    """Make a square lattice with nearest neighbor hopping"""
+    # Return lattice specification for a square lattice with nearest neighbor hoppings
 
     a1 = np.array([1, 0])
     a2 = np.array([0, 1])
@@ -41,21 +40,25 @@ def square_lattice(onsite=(0, 0)):
 
     return lat
 
-
-# load a square lattice
+# load lattice
 lattice = square_lattice()
 # number of decomposition parts in each direction of matrix. This divides the lattice into various sections,
 # each of which is calculated in parallel
 nx = ny = 1
 # number of unit cells in each direction.
 lx = ly = 32
-# Twists in Each Direction
-twsx = twsy = twsz = np.pi/2.0
+# - boundary conditions [mode,mode, ... ] with modes:
+#   . "periodic"
+#   . "open"
+#   . "twist_fixed"     this option needs the extra argument ths=[phi_1,..,phi_DIM] where phi_i \in [0, 2*M_PI]  
+#   . "twist_random"
 # Boundary Mode
 mode = "twisted"
+# Twists in Each Direction
+twsx = twsy = twsz = np.pi/2.0
 
 configuration = kite.Configuration(divisions=[nx, ny], length=[lx, ly], boundaries=[mode,mode], is_complex=True, precision=1, angles=[twsx, twsy])
-# require the calculation of DOS
+# specify calculation type
 calculation = kite.Calculation(configuration)
 calculation.dos(num_points=4000, num_moments=256, num_random=256, num_disorder=1)
 # configure the *.h5 file
